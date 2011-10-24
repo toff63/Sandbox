fe33ebaff4290b8ac26f4227be6ea72d21fb01b1
Christophe Marchal <toff63@gmail.com>
2011-10-24 10:55:19 -0200

    Added code to call clojure functions from java

--- clojure/ldc/test/bank.clj
+++ clojure/ldc/test/bank.clj
@@ -18,3 +18,10 @@
 (deftest test-bank-assessment
   (def ac1 (struct account "Christophe" 1000))
   (is (= "Account: Christophe Amount: 1000" (bank-assessment ac1))))
+
+(deftest test-bank-transfert
+  (def ac1 (ref (struct account "Christophe" 1000)))
+  (def ac2 (ref (struct account "Bill" 110000)))
+  (bank-transfert ac2 ac1 10000)
+  (is (= (struct account "Christophe" 11000) @ac1))
+  (is (= (struct account "Bill" 100000) @ac2)))
