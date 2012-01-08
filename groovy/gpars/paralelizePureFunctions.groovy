e5f610c7d3c1a93a8598fa3d9033c9a8f17d8e42
Christophe Marchal <toff63@gmail.com>
2012-01-08 15:35:52 -0200

    Seperated the creation of asyncFun and closure creation

--- groovy/gpars/paralelizePureFunctions.groovy
+++ groovy/gpars/paralelizePureFunctions.groovy
@@ -3,19 +3,19 @@ import groovyx.gpars.GParsPool
 
 // The idea is to run pure functions (without side effects concurrently)
 
-GParsPool.withPool{
 	def computeSum = { i ->
 		(1 .. i).inject(0, {a, b -> a + 1/(b * b)})
-	}.asyncFun()
+	}
 
 	def Closure getPiFromSum = {sumResult ->
 		Math.sqrt(6 * sumResult)
-	}.asyncFun()
+	}
 
-	def promiseSum = computeSum(100)
+GParsPool.withPool{
+	def promiseSum = computeSum.asyncFun()(100)
 	// the result is a Promise, which is also a DataflowVariable, so you can request its status
 	println "Computation is asynchronous. Are we done yet? " + promiseSum.bound
-	def promiseRes = getPiFromSum(promiseSum.get())
+	def promiseRes = getPiFromSum.asyncFun()(promiseSum.get())
 	promiseRes.whenBound {res -> println res}
 }
 
