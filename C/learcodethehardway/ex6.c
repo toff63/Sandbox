e5071cff32efe9b0707b478cd40c39fc45cf0a51
Christophe Marchal <toff63@gmail.com>
2012-02-27 10:08:30 -0300

    Added exercise 6 of learn  code the hard way

---
+++ C/learcodethehardway/ex6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+	float power = 2.345f;
+	double super_power = 34544.34454;
+	char initial = 'A';
+	char first_name[] = "Zed";
+	char last_name[] = "Shaw";
+
+	printf("You are %d mile away.\n",distance);
+
+	return 0;
+}
