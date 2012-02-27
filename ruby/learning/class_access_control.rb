968770ce480e0b3aaa37fb0d457a28684b82771d
Christophe Marchal <toff63@gmail.com>
2012-02-27 10:31:24 -0300

    Added snippet to specify class access control in ruby

---
+++ ruby/learning/class_access_control.rb
@@ -0,0 +1,49 @@
+class Myclass
+
+	def method1  # default is public
+	end
+
+	protected
+
+	def method2  # protected
+	end
+
+	def method3  # protected
+	end
+
+
+	public
+
+	def method4  # public
+	end
+
+
+	private
+
+	def method5  # private
+	end
+end
+
+
+class Myclass2
+
+	def method1  
+	end
+
+	def method2  
+	end
+
+	def method3  
+	end
+
+
+	def method4  
+	end
+
+	def method5  
+	end
+
+	public :method1, :method4
+	protected :method2, :method3
+	private :method5
+end
