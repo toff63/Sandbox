f8c459a526ae7377caab475455b82fcfcf561072
Christophe Marchal <toff63@gmail.com>
2012-04-02 10:03:24 -0300

    Added exo 5 chap 4 solution scala_impatient

--- scala/scala_impatient/chap4_exercises.scala
+++ scala/scala_impatient/chap4_exercises.scala
@@ -4,37 +4,50 @@ for((gizmo, price) <- gizmos) yield (gizmo, price * 0.9)
 
 2.
 def wordFrequencyMutable(file:String) {
-  val in = new java.util.Scanner(new java.io.File(file))
-  val freq = collection.mutable.Map[String,Int]()
-  while(in.hasNext()){
-      val word = in.next()
-      val f = 1 + freq.getOrElse(word,0)
-      freq += (word -> f)
-  }  
-  print(freq)
+    val in = new java.util.Scanner(new java.io.File(file))
+    val freq = collection.mutable.Map[String,Int]()
+    while(in.hasNext()){
+        val word = in.next()
+        val f = 1 + freq.getOrElse(word,0)
+        freq += (word -> f)
+    }  
+    print(freq)
 }
 
 3.
 def wordFrequencyImmutable(file:String) {
-  val in = new java.util.Scanner(new java.io.File(file))
-  var freq = Map[String,Int]()
-  while(in.hasNext()){
-      val word = in.next()
-      val f = 1 + freq.getOrElse(word,0)
-      freq += (word -> f)
-  }  
-  print(freq)
+    val in = new java.util.Scanner(new java.io.File(file))
+    var freq = Map[String,Int]()
+    while(in.hasNext()){
+        val word = in.next()
+        val f = 1 + freq.getOrElse(word,0)
+        freq += (word -> f)
+    }  
+    print(freq)
 }
 
 4.
 def wordFrequencySortedMap(file:String) {
-  val in = new java.util.Scanner(new java.io.File(file))
-  var freq = collection.immutable.SortedMap[String,Int]()
-  while(in.hasNext()){
-      val word = in.next()
-      val f = 1 + freq.getOrElse(word,0)
-      freq += (word -> f)
-  }  
-  print(freq)
+    val in = new java.util.Scanner(new java.io.File(file))
+    var freq = collection.immutable.SortedMap[String,Int]()
+    while(in.hasNext()){
+        val word = in.next()
+        val f = 1 + freq.getOrElse(word,0)
+        freq += (word -> f)
+    }  
+    print(freq)
 }
 
+5.
+def wordFrequencyTreeMap(file:String){
+    import collection.JavaConversions.mapAsScalaMap
+
+    val in = new java.util.Scanner(new java.io.File(file))
+    val freq : collection.mutable.Map[String,Int] = new java.util.TreeMap[String,Int]
+    while(in.hasNext()){
+        val word = in.next()
+        val f = 1 + freq.getOrElse(word,0)
+        freq += (word -> f)
+    }  
+    print(freq)
+}
