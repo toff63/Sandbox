cb51fa2d21168cea3f7b1b72bd26e7d83fd323cc
Christophe Marchal <toff63@gmail.com>
2012-04-02 10:11:20 -0300

    Added exo 6 chap 4 solution scala_impatient

--- scala/scala_impatient/chap4_exercises.scala
+++ scala/scala_impatient/chap4_exercises.scala
@@ -51,3 +51,14 @@ def wordFrequencyTreeMap(file:String){
     }  
     print(freq)
 }
+
+6.
+val days = collection.mutable.LinkedHashMap("Monday" -> java.util.Calendar.MONDAY,
+                                              "Tuesday" ->java.util.Calendar.TUESDAY,
+                                              "Wednesday" ->java.util.Calendar.WEDNESDAY,
+                                              "Thursday" ->java.util.Calendar.THURSDAY,
+                                              "Friday" ->java.util.Calendar.FRIDAY,
+                                              "Saturday" ->java.util.Calendar.SATURDAY,
+                                              "Sunday" ->java.util.Calendar.SUNDAY)
+
+for((day, calendarDay) <- days) print(day + " is " + calendarDay + "\n")
