cc1142a0136224ea0f0432646b6832b8c9a13da0
Christophe Marchal <toff63@gmail.com>
2012-04-02 09:52:54 -0300

    Added exo 2 chap 4 solution scala_impatient

--- scala/scala_impatient/chap4_exercises.scala
+++ scala/scala_impatient/chap4_exercises.scala
@@ -1,3 +1,15 @@
 1.
 val gizmos=Map("SmartPhone" -> 500, "netbook" -> 600, "workstation" -> 1000)
 for((gizmo, price) <- gizmos) yield (gizmo, price * 0.9)
+
+2.
+def wordFrequency(file:String) {
+  val in = new java.util.Scanner(new java.io.File(file))
+  val freq = collection.mutable.Map[String,Int]()
+  while(in.hasNext()){
+      val word = in.next()
+      val f = 1 + freq.getOrElse(word,0)
+      freq += (word -> f)
+  }  
+  print(freq)
+}
