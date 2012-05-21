51180dddfc220138f3f7484f986e304476514541
Christophe Marchal <toff63@gmail.com>
2012-05-21 14:10:14 -0300

    added 2 other way of computing shop:total using fun and comprehension lists

--- erlang/shop.erl
+++ erlang/shop.erl
@@ -1,7 +1,8 @@
 -module(shop).
--export([cost/1, total/1]).
+-export([cost/1, total/1, total_using_lists/1, total_comp_list/1]).
+-import(lists, [map/2, sum/1]).
 
-cost(oranges) 		-> 5;
+cost(oranges)		-> 5;
 cost(newspaper) -> 8;
 cost(apples) 		-> 2;
 cost(pears) 		-> 9;
@@ -9,3 +10,7 @@ cost(milk) 			-> 7.
 
 total([{What, N} | T]) -> cost(What) * N + total(T);
 total([])              -> 0.
+
+total_using_lists(L) -> sum(map(fun({What, N}) -> cost(What) * N end, L)).
+
+total_comp_list(L) -> sum([cost(What) * N || {What, N} <- L]).
