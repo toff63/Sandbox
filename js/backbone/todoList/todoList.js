593e9e3ba775c45a0901248a19242ab33d5ab409
Christophe Marchal <toff63@gmail.com>
2012-08-04 17:07:28 -0300

    Refactored todoList.js so I display a list of model objects

--- js/backbone/todoList/todoList.js
+++ js/backbone/todoList/todoList.js
@@ -1,5 +1,15 @@
 (function(){
 
+	var Item = Backbone.Model.extend({
+		defaults: {
+			part1: 'hello',
+			part2: 'world'
+		}
+	});
+
+	var List = Backbone.Collection.extend({
+		model: Item
+	});
 
 	var ListView = Backbone.View.extend({
 		el: $('body'), // attach this.el to an existing element
@@ -9,20 +19,35 @@
 		},
 
 		initialize: function () {
-			_.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
+			_.bindAll(this, 'render', 'addItem', 'appendItem'); // fixes loss of context for 'this' within methods
+
+			this.collection = new List();
+			this.collection.bind('add', this.appendItem); // collection event binder
 
 			this.counter = 0;
 			this.render(); // so it renders itself :)
 		},
 
 		render: function(){
+			var self = this;
 			$(this.el).append('<button id="add">Add list item</button>');
 			$(this.el).append("<ul></ul>");
+			_(this.collection.models).each(function(item) {
+				self.appendItem(item);
+			}, this);
 		},
 
 	  addItem: function(){
 			this.counter++;
-			$('ul', this.el).append('<li>hell world ' + this.counter + '</li>');
+			var item = new Item();
+			item.set({
+				part2: item.get('part2') + ' ' + this.counter // modify the default object
+			});
+			this.collection.add(item); // add item to collection: view is updated via event 'add'
+		},
+
+		appendItem: function(item){
+			$('ul', this.el).append('<li>' + item.get('part1') + ' ' + item.get('part2') + '</li>');						
 		}
 	});
 
