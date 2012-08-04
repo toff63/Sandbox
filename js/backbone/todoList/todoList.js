a0b4e4fc68d95f8b2dfb047f7add09c4fb404c88
Christophe Marchal <toff63@gmail.com>
2012-08-04 16:56:37 -0300

    Added a button and its event handler

--- js/backbone/todoList/todoList.js
+++ js/backbone/todoList/todoList.js
@@ -1,14 +1,28 @@
 (function(){
+
+
 	var ListView = Backbone.View.extend({
 		el: $('body'), // attach this.el to an existing element
 
+		events: {
+			'click button#add': 'addItem'
+		},
+
 		initialize: function () {
 			_.bindAll(this, 'render'); // fixes loss of context for 'this' within methods
+
+			this.counter = 0;
 			this.render(); // so it renders itself :)
 		},
 
 		render: function(){
-			$(this.el).append("<ul><li>hello world</li></ul>");
+			$(this.el).append('<button id="add">Add list item</button>');
+			$(this.el).append("<ul></ul>");
+		},
+
+	  addItem: function(){
+			this.counter++;
+			$('ul', this.el).append('<li>hell world ' + this.counter + '</li>');
 		}
 	});
 
