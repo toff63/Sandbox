7a19e0961da12d7e1841b092f6e550f1fca081a4
Christophe Marchal <toff63@gmail.com>
2012-08-31 10:09:33 -0300

    Added a webtail with nodejs

---
+++ js/nodejs/node_test/webtail.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var spawn = require('child_process').spawn;
+
+http.createServer(function(request, response){
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    var tail_child = spawn('tail', ['-f', '/var/log/syslog']);
+    request.connection.on('end', function(){
+        tail_child.kill();
+    })
+    tail_child.stdout.on('data', function(data){
+        console.log(data.toString());
+        response.write(data);
+    });
+}).listen(4000);
