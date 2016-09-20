04c86300de41657a6d393cdf984247b26b71c71c
Christophe Marchal <toff63@gmail.com>
2016-09-19 23:02:59 -0300

    Add strcut to go project

--- go/helloworld/src/plant.go
+++ go/helloworld/src/plant.go
@@ -1,41 +1,49 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	menu()
+	plants := []PowerPlant{
+		PowerPlant{hydro, 300, active},
+		PowerPlant{wind, 30, active},
+		PowerPlant{wind, 25, inactive},
+		PowerPlant{wind, 35, active},
+		PowerPlant{solar, 45, unavailable},
+		PowerPlant{solar, 40, inactive},
+	}
+	grid := PowerGrid{300, plants}
+
+	if option, err := menu(); err == nil {
+
+		fmt.Println("")
+		switch option {
+		case "1":
+			grid.generatePlantReport()
+		case "2":
+			grid.generateGridReport()
+		}
+
+	} else {
+		fmt.Println(err.Error())
+	}
 
 }
 
-func menu() {
-	plantCapacities := []float64{30, 30, 30, 60, 60, 100}
-	activePlants := []int{0, 1}
-	gridLoad := 75.
-
+func menu() (option string, err error) {
 	fmt.Println("1) Generate Power Plant Report")
 	fmt.Println("2) Generate Power Grid Report")
 	fmt.Println("Please choose an option: ")
 
-	var option string
-
 	fmt.Scanln(&option)
-
-	switch option {
-	case "1":
-		powerPlantReport(plantCapacities)
-	case "2":
-		powerGridReport(plantCapacities, activePlants, gridLoad)
-	default:
-		fmt.Println("Unknown option.")
+	if option != "1" && option != "2" {
+		err = errors.New("Invalid option selected")
 	}
-}
 
-func powerPlantReport(plantCapacities []float64) {
-	for index, capacity := range plantCapacities {
-		fmt.Printf("Plant %d capacity: %.0f\n", index, capacity)
-	}
+	return
 }
 
 func powerGridReport(plantCapacities []float64, activePlants []int, gridLoad float64) {
@@ -47,4 +55,61 @@ func powerGridReport(plantCapacities []float64, activePlants []int, gridLoad flo
 	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
 	fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
 	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
+}
+
+type PlantType string
+
+const (
+	hydro PlantType = "Hydro"
+	wind  PlantType = "Wind"
+	solar PlantType = "Solar"
+)
+
+type PlantStatus string
+
+const (
+	active      PlantStatus = "Active"
+	inactive    PlantStatus = "Inactive"
+	unavailable PlantStatus = "Unavailable"
+)
+
+type PowerPlant struct {
+	plantType PlantType
+	capacity  float64
+	status    PlantStatus
+}
+
+type PowerGrid struct {
+	load   float64
+	plants []PowerPlant
+}
+
+func (pg *PowerGrid) generatePlantReport() {
+	for index, plant := range pg.plants {
+		label := fmt.Sprintf("%s%d", "Plant #", index)
+		printReportLabel(label)
+		fmt.Printf("%-20s%s\n", "Type: ", plant.plantType)
+		fmt.Printf("%-20s%.0f\n", "Capacity: ", plant.capacity)
+		fmt.Printf("%-20s%s\n", "Status: ", plant.status)
+		fmt.Println("")
+	}
+}
+
+func printReportLabel(label string) {
+	fmt.Println(label)
+	fmt.Println(strings.Repeat("-", len(label)))
+}
+
+func (pg *PowerGrid) generateGridReport() {
+	capacity := 0.
+	for _, plant := range pg.plants {
+		if plant.status == active {
+			capacity += plant.capacity
+		}
+	}
+	printReportLabel("Power Grid Report")
+
+	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+	fmt.Printf("%-20s%.0f\n", "Load: ", pg.load)
+	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", pg.load/capacity*100)
 }
