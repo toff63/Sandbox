0b74b9823bf7b9f1a08d9046ba4bee9427380ff7
Christophe Marchal <toff63@gmail.com>
2021-04-02 21:26:33 +0100

    First tailwindcss setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      'display': ['Lato', 'La belle Aurore'],
+      'body': ['Lato', 'La belle Aurore'],
+    }
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const plugins = [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+        filename: isProduction ? '[name].[hash].css' : '[name].css'
+    }),
+    new WebpackManifestPlugin({
+        fileName: '../_data/manifest.yml',
+        publicPath: './dist/',
+    }),
+]
+
+if (!isProduction) {
+    // only enable hot in development
+    plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+
+module.exports = {
+    mode: isProduction ? 'production' : 'development',
+    entry: {
+        docs: path.resolve(__dirname, './assets/index.css'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist/'),
+        filename: isProduction ? '[name].[hash].js' : '[name].js',
+        chunkFilename: isProduction ? '[id].[hash].js' : '[id].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                  {
+                    loader: MiniCssExtractPlugin.loader,
+                  },
+                  'css-loader',
+                  'postcss-loader',
+                ]
+            }
+        ]
+    },
+    plugins: plugins,
+};
+
